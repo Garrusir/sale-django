07f7d6e6be86dbb1c9551670eea68c44abe4e82a
Garrusir <urbansparrow1256@gmail.com>
2021-06-25 23:41:32 +0300

    add catalog page and sale card

--- frontend/src/pages/CatalogPage.js
+++ frontend/src/pages/CatalogPage.js
@@ -1,7 +1,23 @@
 import React, {useEffect, useState} from 'react';
+import SaleCard from '../components/SaleCard';
+import {Grid} from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
 
 export function CatalogPage() {
     const [sales, setSales] = useState([]);
+    const classes = useStyles();
 
     useEffect( () => {
         // const result = await fetch('api/sales');
@@ -20,18 +36,19 @@ export function CatalogPage() {
             })
     }, [])
 
-
     return (
-        <div>
-            <h1>Страница со списком скидок</h1>
-            <ul>
+        <div className={classes.root}>
+            <Typography variant="h6" color="inherit" noWrap className="toolbarTitle">
+                    Скидки и акциии <span role="img" aria-label="Shopping Cart">🛒</span>
+            </Typography>
+            <Grid container spacing={3}>
                 {
                     sales.map(item =>
-                        (<li key={item.id}>
-                            {item.title}
-                        </li>))
+                        (<Grid item xs={4}>
+                            <SaleCard key={item.id} sale={item} />
+                        </Grid>))
                 }
-            </ul>
+            </Grid>
         </div>
     )
 }
